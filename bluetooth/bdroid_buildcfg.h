b205eafac08b5c25487b480898e7a2b7a7e21104
zac6ix <nanaquameallday@gmail.com>
2017-10-23 04:22:17 +0000

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Moto E4"
+#define BTM_DEF_LOCAL_NAME   "Moto E4 Plus"
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 
 #endif
